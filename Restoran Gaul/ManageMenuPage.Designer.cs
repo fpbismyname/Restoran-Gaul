d263cfdd06558dc3a31e27afedc75debe65ada87
fpbismyname <fajarpebefajar12@gmail.com>
2023-04-07 20:28:41 +0700

    this is

--- Restoran Gaul/ManageMenuPage.Designer.cs
+++ Restoran Gaul/ManageMenuPage.Designer.cs
@@ -28,20 +28,258 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cari_menu = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.daftar_menu = new System.Windows.Forms.DataGridView();
+            this.menu_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nama_menu = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.harga_menu = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.photo_menu = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.carbo_menu = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.protein_menu = new System.Windows.Forms.TextBox();
+            this.insert_menu = new System.Windows.Forms.Button();
+            this.delete_menu = new System.Windows.Forms.Button();
+            this.update_menu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.daftar_menu)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Golden Smile", 24F);
+            this.label1.Location = new System.Drawing.Point(4, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Form Manage Menu";
+            // 
+            // cari_menu
+            // 
+            this.cari_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.cari_menu.Location = new System.Drawing.Point(540, 46);
+            this.cari_menu.Name = "cari_menu";
+            this.cari_menu.Size = new System.Drawing.Size(166, 28);
+            this.cari_menu.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.button1.Location = new System.Drawing.Point(712, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // daftar_menu
+            // 
+            this.daftar_menu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.daftar_menu.Location = new System.Drawing.Point(12, 92);
+            this.daftar_menu.Name = "daftar_menu";
+            this.daftar_menu.Size = new System.Drawing.Size(775, 246);
+            this.daftar_menu.TabIndex = 3;
+            // 
+            // menu_id
+            // 
+            this.menu_id.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.menu_id.Location = new System.Drawing.Point(336, 362);
+            this.menu_id.Name = "menu_id";
+            this.menu_id.Size = new System.Drawing.Size(166, 28);
+            this.menu_id.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.label2.Location = new System.Drawing.Point(258, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Menu Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.label3.Location = new System.Drawing.Point(258, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Name";
+            // 
+            // nama_menu
+            // 
+            this.nama_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.nama_menu.Location = new System.Drawing.Point(336, 396);
+            this.nama_menu.Name = "nama_menu";
+            this.nama_menu.Size = new System.Drawing.Size(166, 28);
+            this.nama_menu.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.label4.Location = new System.Drawing.Point(258, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 23);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Price";
+            // 
+            // harga_menu
+            // 
+            this.harga_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.harga_menu.Location = new System.Drawing.Point(336, 430);
+            this.harga_menu.Name = "harga_menu";
+            this.harga_menu.Size = new System.Drawing.Size(166, 28);
+            this.harga_menu.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.label5.Location = new System.Drawing.Point(258, 467);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 23);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Photo";
+            // 
+            // photo_menu
+            // 
+            this.photo_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.photo_menu.Location = new System.Drawing.Point(336, 464);
+            this.photo_menu.Name = "photo_menu";
+            this.photo_menu.Size = new System.Drawing.Size(166, 28);
+            this.photo_menu.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.label6.Location = new System.Drawing.Point(258, 501);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 23);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Carbo";
+            // 
+            // carbo_menu
+            // 
+            this.carbo_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.carbo_menu.Location = new System.Drawing.Point(336, 498);
+            this.carbo_menu.Name = "carbo_menu";
+            this.carbo_menu.Size = new System.Drawing.Size(166, 28);
+            this.carbo_menu.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.label7.Location = new System.Drawing.Point(258, 535);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 23);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Protein";
+            // 
+            // protein_menu
+            // 
+            this.protein_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.protein_menu.Location = new System.Drawing.Point(336, 532);
+            this.protein_menu.Name = "protein_menu";
+            this.protein_menu.Size = new System.Drawing.Size(166, 28);
+            this.protein_menu.TabIndex = 1;
+            // 
+            // insert_menu
+            // 
+            this.insert_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.insert_menu.Location = new System.Drawing.Point(262, 585);
+            this.insert_menu.Name = "insert_menu";
+            this.insert_menu.Size = new System.Drawing.Size(75, 32);
+            this.insert_menu.TabIndex = 2;
+            this.insert_menu.Text = "Insert";
+            this.insert_menu.UseVisualStyleBackColor = true;
+            this.insert_menu.Click += new System.EventHandler(this.insert_menu_Click);
+            // 
+            // delete_menu
+            // 
+            this.delete_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.delete_menu.Location = new System.Drawing.Point(427, 585);
+            this.delete_menu.Name = "delete_menu";
+            this.delete_menu.Size = new System.Drawing.Size(75, 32);
+            this.delete_menu.TabIndex = 2;
+            this.delete_menu.Text = "Delete";
+            this.delete_menu.UseVisualStyleBackColor = true;
+            // 
+            // update_menu
+            // 
+            this.update_menu.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.update_menu.Location = new System.Drawing.Point(345, 585);
+            this.update_menu.Name = "update_menu";
+            this.update_menu.Size = new System.Drawing.Size(75, 32);
+            this.update_menu.TabIndex = 2;
+            this.update_menu.Text = "Update";
+            this.update_menu.UseVisualStyleBackColor = true;
             // 
             // ManageMenuPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 654);
+            this.Controls.Add(this.daftar_menu);
+            this.Controls.Add(this.update_menu);
+            this.Controls.Add(this.delete_menu);
+            this.Controls.Add(this.insert_menu);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.protein_menu);
+            this.Controls.Add(this.carbo_menu);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.photo_menu);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.harga_menu);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nama_menu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.menu_id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cari_menu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "ManageMenuPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ManageMenuPage";
+            ((System.ComponentModel.ISupportInitialize)(this.daftar_menu)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cari_menu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView daftar_menu;
+        private System.Windows.Forms.TextBox menu_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nama_menu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox harga_menu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox photo_menu;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox carbo_menu;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox protein_menu;
+        private System.Windows.Forms.Button insert_menu;
+        private System.Windows.Forms.Button delete_menu;
+        private System.Windows.Forms.Button update_menu;
     }
 }
--- Restoran Gaul/ManageMenuPage.cs
+++ Restoran Gaul/ManageMenuPage.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Restoran_Gaul
@@ -15,6 +10,62 @@ namespace Restoran_Gaul
         public ManageMenuPage()
         {
             InitializeComponent();
+        }
+
+        //searching menu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constring = "server= localhost; database= db_restoran; uid= root; pwd= Fajar12BuDiman;";
+            MySqlConnection con = new MySqlConnection(constring);
+            con.Open();
+            try
+            {
+                string query = "select * from msmenu where Name like '%" + cari_menu.Text + "%'";
+                MySqlDataAdapter sda = new MySqlDataAdapter(query, con);
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+                daftar_menu.DataSource = dataTable;
+                daftar_menu.ReadOnly = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            { con.Close(); }
+        }
+
+        private void insert_menu_Click(object sender, EventArgs e)
+        {
+            string constring = "server= localhost; database= db_restoran; uid= root; pwd= Fajar12BuDiman;";
+            MySqlConnection con = new MySqlConnection(constring);
+            con.Open();
+
+            try
+            {
+                string query = "insert into msmenu(Id, Name, Price, Photo, Carbo, Protein) Values(" + menu_id.Text + ",'" + nama_menu.Text + "'," + harga_menu.Text + ",'" + photo_menu.Text + "'," + carbo_menu.Text + "," + protein_menu.Text + ");";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    MessageBox.Show("Menu telah ditambahkan !", "Yeay..");
+                    menu_id.Clear();
+                    nama_menu.Clear();
+                    harga_menu.Clear();
+                    photo_menu.Clear();
+                    carbo_menu.Clear();
+                    protein_menu.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex != null)
+                {
+                    MessageBox.Show("Menu baru tidak boleh kosong !", "Ops..");
+                }
+            }
+            finally
+            { con.Close(); }
         }
     }
 }
